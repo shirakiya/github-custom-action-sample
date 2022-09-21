7e588ef1ee47ff0e18f8e751e386144422529abe
shirakiya <shirakiya.pv@gmail.com>
2022-09-22 00:59:32 +0900

    introduce prettier

--- dist/main.js
+++ dist/main.js
@@ -1,51 +1,51 @@
-const core = require("@actions/core");
-const github = require("@actions/github");
-const glob = require("@actions/glob");
-const exec = require("@actions/exec");
+const core = require("@actions/core")
+const github = require("@actions/github")
+const glob = require("@actions/glob")
+const exec = require("@actions/exec")
 
 const run = async () => {
-  const name = core.getInput("name");
-  console.log(`Hello ${name}!`);
+  const name = core.getInput("name")
+  console.log(`Hello ${name}!`)
 
-  const payload = JSON.stringify(github.context.payload, null, 2);
-  console.log(`The event payload: ${payload}`);
+  const payload = JSON.stringify(github.context.payload, null, 2)
+  console.log(`The event payload: ${payload}`)
 
-  await exec.exec("pwd");
+  await exec.exec("pwd")
 
-  const globber = await glob.create("**/README.md");
+  const globber = await glob.create("**/README.md")
   for await (const file of globber.globGenerator()) {
     if (file.includes("node_modules")) {
-      continue;
+      continue
     }
 
-    let stdout = "";
-    let stderr = "";
+    let stdout = ""
+    let stderr = ""
 
     const options = {
       listeners: {
         stdout: (data) => {
-          stdout += data.toString();
+          stdout += data.toString()
         },
         stderr: (data) => {
-          stderr += data.toString();
+          stderr += data.toString()
         },
       },
       cwd: file.replace("README.md", ""),
-    };
+    }
 
-    await exec.exec("tree", ["."], options);
+    await exec.exec("tree", ["."], options)
 
-    console.log("stdout: ", stdout);
-    console.log("stderr: ", stderr);
+    console.log("stdout: ", stdout)
+    console.log("stderr: ", stderr)
   }
-};
+}
 
 const main = async () => {
   try {
-    await run();
+    await run()
   } catch (e) {
-    core.setFailed(e.message);
+    core.setFailed(e.message)
   }
-};
+}
 
-main();
+main()
